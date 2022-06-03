02f39ba9661cd2fd649acf9e3baeb77b91711097
OnurGuven35 <gvnn.onrr@gmail.com>
2022-06-03 14:14:49 +0300

    Initial commit

---
+++ src/Divisors.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Divisors {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int num, ort, top = 0, sayac = 0;
+        System.out.print("Lutfen Sayiyi Giriniz : ");
+        num = inp.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                top += i;
+                sayac++;
+            }
+        }
+
+        if (num <= 0) {
+            System.out.print("Lutfen Gecerli Bir Deger Giriniz ! ");
+        } else {
+            ort = top / sayac;
+            System.out.print("Girdiginiz Sayiya Kadar Olan Sayilarin Icinde 3'e ve 4'e Bolunen Sayilarin Ortalamasi : " + ort);
+        }
+    }
+}
+
+
+
+
+
+
